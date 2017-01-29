753e3433c878b809b7dfcb67965ff2e85074c6cf
Pat Falconer <Patrick.j.Falconer@gmail.com>
2017-01-29 16:19:09 -0600

    completed outline of cart and item page functions with html and views

--- apps/wootapp/views.py
+++ apps/wootapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from models import *
 
 def index(request):
     return render(request, 'wootapp/index.html')
@@ -9,8 +10,18 @@ def browse(request):
 def create_deal(request):
     return render(request, 'wootapp/create_deal.html')
 def cart(request):
-    return render(request, 'wootapp/cart.html')
-def item(request):
-    return render(request, 'wootapp/item.html')
+	id = request.session['id']
+	cart = Carts.objects.get(user_id=id)
+	rating = "1"
+	context = {'cart':cart, 'rating':rating}
+	return render(request, 'wootapp/cart.html', context)
+def item(request, id):
+	try:
+		item = Items.objects.get(id=id)
+		discussion = Discussion.objects.get(item_id=id).order_by('-created_at')
+		context = {'item': item, 'discussion': discussion}
+		return render(request, 'wootapp/item.html', context)
+	except:
+		return redirect('/')
 def checkout(request):
     return render(request, 'wootapp/checkout.html')
