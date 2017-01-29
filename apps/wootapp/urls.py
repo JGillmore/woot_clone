e47f4107b2bebb6e6eebc72f1a943b7d9b28d3db
Jeremy Gillmore <happy12287@gmail.com>
2017-01-29 14:24:18 -0600

    added routes and views for each html page

--- apps/wootapp/urls.py
+++ apps/wootapp/urls.py
@@ -3,4 +3,10 @@ from views import *
 
 urlpatterns = [
 	url(r'^$', index, name='index'),
+	url(r'^user$', user, name='user'),
+	url(r'^browse/(?P<category>.+$)', browse, name='browse'),
+	url(r'^create$', create_deal, name='create_deal'),
+	url(r'^item/(?P<id>\d+$)', item, name='item'),
+	url(r'^cart$', cart, name='cart'),
+	url(r'^checkout$', checkout, name='checkout'),
 ]
