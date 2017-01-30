34dbc21611486c3676ff21c899eb87cebf2f680c
Nick Phillips <phillipn101@gmail.com>
2017-01-30 17:16:50 -0600

    Add stripe info. to Product model

--- apps/wootapp/models.py
+++ apps/wootapp/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from ..loginapp.models import *
+import settings
 
 class ItemsManager(models.Manager):
 	def validate(self, name, description, price, units, category, image):
@@ -80,29 +81,52 @@ class Ratings(models.Model):
 	item = models.ForeignKey(Items, related_name='item_rating')
 	objects = RatingsManger()
 
-class CCManager(models.Manager):
-	def validate(self):
-		pass
-	def add(self):
-		pass
-	def update(self):
-		pass
-	def delete():
-		pass
+class Purchases(models.Model):
+	def __init__(self, *args, **kwargs):
+		super(Purchases, self).__init__(*args, **kwargs)
 
-class CC(models.Model):
-	full_name = models.CharField(max_length=100)
-	card_number = models.IntegerField()
-	expiration_date = models.DateField()
-	security_code = models.PositiveSmallIntegerField()
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
-	user = models.ForeignKey(Users, related_name='user_cc')
-	objects = CCManager()
+		import stripe
+		stripe.api_key = settings.STRIPE_API_KEY
 
-class Purchased(models.Model):
+		self.stripe = stripe
+
 	item = models.ForeignKey(Items, related_name='item_purchased')
 	user = models.ForeignKey(Users, related_name='user_purchased')
+	status = models.CharField(max_length=255)
+	charge_id = models.CharField(max_length=32, default='')
 	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
-		
+	def charge(self, price_in_cents, number, exp_month, exp_year, cvc):
+		# Takes a the price and credit card details: number, exp_month, exp_year, cvc. Returns a tuple: (Boolean, Class) where the boolean is if the charge was successful, and the class is response (or error) instance.
+	    if self.charge_id: # don't let this be charged twice!
+	        return False, Exception(message="Already charged.")
+
+	      	try:
+	          	response = self.stripe.Charge.create(
+				amount = price_in_cents,
+				currency = "usd",
+				card = {
+				  "number" : number,
+				  "exp_month" : exp_month,
+				  "exp_year" : exp_year,
+				  "cvc" : cvc,
+
+				  #### it is recommended to include the address!
+				  #"address_line1" : self.address1,
+				  #"address_line2" : self.address2,
+				  #"address_zip" : self.zip_code,
+				  #"address_state" : self.state,
+				},
+				description='Thank you for your purchase!')
+
+	        	self.charge_id = response.id
+
+	      	except self.stripe.CardError, ce:
+	          # charge failed
+	        	print ce
+	        	return False, ce
+
+	      	self.status = 'closed'
+	      	self.save()
+	      	return True, response
---
+++ apps/wootapp/settings.py
@@ -0,0 +1,1 @@
+STRIPE_API_KEY = 'sk_test_SpqjGqqF7BdfdmMMCn8htPWx'
