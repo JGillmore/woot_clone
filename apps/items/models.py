2225196084c2238a22e7363f9a480cab1e907bf1
Nick Phillips <phillipn101@gmail.com>
2017-01-31 15:45:02 -0600

    add payment functionality

--- apps/items/models.py
+++ apps/items/models.py
@@ -41,7 +41,7 @@ class Purchases(models.Model):
 		# HEY GUYS! pip install --index-url https://code.stripe.com --upgrade stripe
 
 	item = models.ForeignKey(Items, related_name='item_purchased')
-	user = models.ForeignKey(Users, related_name='user_purchased')
+	user = models.ForeignKey(Users)
 	status = models.CharField(max_length=255)
 	charge_id = models.CharField(max_length=32, default='')
 	created_at = models.DateTimeField(auto_now_add=True)
@@ -49,37 +49,36 @@ class Purchases(models.Model):
 
 	def charge(self, price_in_cents, number, exp_month, exp_year, cvc):
 		# Takes a the price and credit card details: number, exp_month, exp_year, cvc. Returns a tuple: (Boolean, Class) where the boolean is if the charge was successful, and the class is response (or error) instance.
-	    if self.charge_id: # don't let this be charged twice!
-	        return False, Exception(message="Already charged.")
+		if self.charge_id: # don't let this be charged twice!
+			return False, Exception(message="Already charged.")
 
-	      	try:
-	          	response = self.stripe.Charge.create(
-				amount = price_in_cents,
-				currency = "usd",
-				card = {
-				  "number" : number,
-				  "exp_month" : exp_month,
-				  "exp_year" : exp_year,
-				  "cvc" : cvc,
+		try:
+		  	response = self.stripe.Charge.create(
+			amount = price_in_cents,
+			currency = "usd",
+			card = {
+			  "number" : number,
+			  "exp_month" : exp_month,
+			  "exp_year" : exp_year,
+			  "cvc" : cvc,
 
-				  #### it is recommended to include the address!
-				  #"address_line1" : self.address1,
-				  #"address_line2" : self.address2,
-				  #"address_zip" : self.zip_code,
-				  #"address_state" : self.state,
-				},
-				description='Thank you for your purchase!')
+			  #### it is recommended to include the address!
+			  #"address_line1" : self.address1,
+			  #"address_line2" : self.address2,
+			  #"address_zip" : self.zip_code,
+			  #"address_state" : self.state,
+			},
+			description='Thank you for your purchase!')
 
-	        	self.charge_id = response.id
+			self.charge_id = response.id
 
-	      	except self.stripe.CardError, ce:
-	          # charge failed
-	        	print ce
-	        	return False, ce
+		except self.stripe.CardError, ce:
+			print ce
+			return False, ce
 
-	      	self.status = 'closed'
-	      	self.save()
-	      	return True, response
+		self.status = 'closed'
+		self.save()
+		return True, response
 
 class DiscussionsManager(models.Manager):
 	def validate(self):
