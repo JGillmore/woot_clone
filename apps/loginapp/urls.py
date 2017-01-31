2635735f4fbed29f316f563c02b68cfa88519d61
Nick Phillips <phillipn101@gmail.com>
2017-01-30 20:38:18 -0600

    Clean some more

--- apps/loginapp/urls.py
+++ apps/loginapp/urls.py
@@ -4,9 +4,9 @@ from views import *
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'^user$', user, name='user'),
     url(r'^login$',login, name='login'),
-    url(r'^logout$',logout, name='logout'),
     url(r'^register$',register, name='register'),
+    url(r'^logout$',logout, name='logout'),
+    url(r'^profile$', profile, name='profile'),
     url(r'^update_info$', update_info, name='update_info'),
 ]
--- apps/loginapp/views.py
+++ apps/loginapp/views.py
@@ -5,21 +5,14 @@ from django.contrib.messages import get_messages
 from django.core.urlresolvers import reverse
 from datetime import date
 
+from ..wootapp.models import Items
+
 def index(request):
     context = {}
     request.session['restrictday']=str(date.today())
     if messages:
         context['messages']=get_messages(request)
     return render(request, 'loginapp/index.html', context)
-
-def user(request):
-    if 'id' in request.session:
-        user = Users.objects.get(id=request.session['id'])
-        purchased_items = Items.objects.filter(item_purchased__status='closed').filter(item_purchased__user=user)
-        birth_date = str(user.birth_date)
-        context = {'user':user, 'birth_date':birth_date, 'purchased_items':purchased_items}
-        return render(request, 'wootapp/user.html', context)
-    return redirect('login:login')
 
 def login(request):
     if request.method == 'POST':
@@ -28,11 +21,7 @@ def login(request):
         if Users.objects.login(request,email,password):
             request.session['today']=date.today().strftime('%b %d, %Y')
             request.session['restrictday']=str(date.today())
-            return redirect(reverse('woot:index'))
-    return redirect('/')
-
-def logout(request):
-    request.session.clear()
+            return redirect(reverse('login:profile'))
     return redirect('/')
 
 def register(request):
@@ -49,8 +38,21 @@ def register(request):
             request.session['id']=temp.id
             request.session['today']=date.today().strftime('%b %d, %Y')
             request.session['restrictday']=str(date.today())
-            return redirect(reverse('woot:index'))
+            return redirect(reverse('login:profile'))
         return redirect('/')
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
+
+def profile(request):
+    if 'id' in request.session:
+        user = Users.objects.get(id=request.session['id'])
+        purchased_items = Items.objects.filter(item_purchased__status='closed').filter(item_purchased__user=user)
+        birth_date = str(user.birth_date)
+        context = {'user':user, 'birth_date':birth_date, 'purchased_items':purchased_items}
+        return render(request, 'wootapp/user.html', context)
+    return redirect('login:login')
 
 def update_info(request):
     if request.method=='POST':
