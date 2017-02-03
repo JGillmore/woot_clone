57439909b60f8a57f58ae23d18c9c440c81fae34
Nick Phillips <phillipn101@gmail.com>
2017-02-03 11:00:22 -0600

    remove debug toolbar

--- woot_clone/settings.py
+++ woot_clone/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
+    #'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -120,7 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-INTERNAL_IPS = ('127.0.0.1',)
+#INTERNAL_IPS = ('127.0.0.1',)
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ( os.path.join('static'), )
