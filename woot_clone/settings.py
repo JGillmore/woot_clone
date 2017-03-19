94f851abd544ed59f433db744627ede2c355356c
Nick Phillips <phillipn101@gmail.com>
2017-03-19 16:59:04 -0500

    revise static root

--- woot_clone/settings.py
+++ woot_clone/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ( os.path.join('assets'), )
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
